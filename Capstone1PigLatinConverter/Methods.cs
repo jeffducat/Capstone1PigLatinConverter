09008dc7873f8615f249867639c53bbffffc9157
jeff_ducat <jducat81@gmail.com>
2019-04-28 20:25:40 -0400

    This is Capstone1 Pig Latin Converter

---
+++ Capstone1PigLatinConverter/Methods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone1PigLatinConverter
+{
+    class Methods
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="option1"></param>
+        /// <param name="option2"></param>
+        /// <returns></returns>
+        public static bool Continue(string message, string option1, string option2)
+        {
+            Console.Write(message);
+            string response = Console.ReadLine();
+
+            if (response == option1)
+            {
+                return true;
+            }
+            else if (response == option2)
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry!");
+                return Continue(message, option1, option2);
+            }
+
+        }
+    }
+}
---
+++ Capstone1PigLatinConverter/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone1PigLatinConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run;
+            string vowels = "aeiou";
+            string invalidCharacters = "1234567890!@#$%^&*()";
+
+            Console.WriteLine("Welcome to the English to Pig Latin Converter!");
+
+            run = true;
+
+            while (run)
+            {
+                Console.WriteLine("Please enter a word to translate: ");
+
+                string input = Console.ReadLine().ToLower();
+                List<char> letters = input.ToList();
+                Console.WriteLine("\nPig Latin Translation:");
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (vowels.Contains(input.Substring(i, 1)))
+                    {
+                        string letter = i.Equals(0) ? "w" : "";                        
+
+                        string prefix = input.Substring(0, i) + letter;
+                        string suffix = input.Substring(i);
+
+                        input = suffix + prefix + "ay";
+                        break;
+                    }
+                    else if (invalidCharacters.Contains(input.Substring(i, 1)))
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine(input);
+                run = Methods.Continue("\nWould you like to translate another word? ( y / n)", "y", "n");
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Thanks for using the Pig Latin Converter!  Goodbye!");
+            Console.ReadLine();
+
+        }
+    }
+}
